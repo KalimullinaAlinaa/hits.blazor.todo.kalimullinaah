8dfd277b487af33d650370e7ea7c22153db0f549
Alina Kalimllina <boulevarovaalina@icloud.com>
2024-09-29 21:59:13 +0500

    Task 1 Создание таблицы

---
+++ Todo.ServerApp/Data/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo.ServerApp.Data
+{
+	public class TaskItem
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public DateTime? FinishDate { get; set; }
+
+	}
+}
