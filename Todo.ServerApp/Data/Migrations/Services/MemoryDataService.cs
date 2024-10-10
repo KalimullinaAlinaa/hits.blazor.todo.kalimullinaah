e0e4d4e9d0be5c8b836b689167ab51fa2703dea5
Alina Kalimllina <boulevarovaalina@icloud.com>
2024-10-10 13:08:12 +0500

    Revert "Task 3"
    This reverts commit 14f46ed618f6c6573d242e4c7ffbedfbef394c74.

--- Todo.ServerApp/Data/Migrations/Services/MemoryDataService.cs
+++ Todo.ServerApp/Data/Migrations/Services/MemoryDataService.cs
@@ -6,9 +6,9 @@ namespace Todo.ServerApp.Data.Migrations.Services
     {
         private static IEnumerable<TaskItem> tasks =
 		[
-			new () {Id =1, Title="Задача 1",Description="Описание задачи 1",CreatedDate=DateTime.Now },
-            new () {Id =2, Title="Задача 2",Description="Описание задачи 2",CreatedDate=DateTime.Now },
-            new () {Id =3, Title="Задача 3",Description="Описание задачи 3",CreatedDate=DateTime.Now },
+			new () {Id =1, Title="Задача 1",Description="Описание задачи 1",CreatedDate=DateTime.Now,},
+            new () {Id =2, Title="Задача 2",Description="Описание задачи 2",CreatedDate=DateTime.Now,},
+            new () {Id =3, Title="Задача 3",Description="Описание задачи 3",CreatedDate=DateTime.Now,},
         ];
         public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
         {
